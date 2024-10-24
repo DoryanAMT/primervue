9be37154eb4d70bba7ae722b5efcab89861d6005
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2024-10-24 14:17:52 +0200

    24/10/2024 mi primer vue

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Mi primera App de Vue</h1>
+    </div>
+</template>
+
+<script>
+    //  ESTO ES CODIGO JS (ESPECIAL)
+    //  CADA COMPONENT QUE CREEMOS SIMPRE DEBEMOS EXPORTARLO
+    //  CON UN name
+    export default {
+        name: "HolaMundo"
+    }
+
+</script>
+
+<style>
+    h1{color: fuchsia;}
+</style>
